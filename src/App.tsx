907b884d2a1e2b8ec6e866880413178fbb21c8ba
asta-otaku <afolabiibrahim08@gmail.com>
2024-05-14 07:12:13 +0100

    fix: animation

--- src/App.tsx
+++ src/App.tsx
@@ -12,25 +12,24 @@ function App() {
     setTimeout(() => {
       setStep(nextStep);
       setTransitioning(false);
-    }, 1000); // Adjust the delay time as needed
+    }, 500); // Adjust the delay time as needed
   };
 
   return (
-    <div className="flex flex-col w-screen h-screen items-center justify-center bg-white p-4 md:p-8">
-      <a href="/" className="absolute top-9">
+    <div className="flex flex-col w-screen h-screen overflow-hidden items-center justify-center bg-white p-4 md:p-8">
+      <a href="/" className="absolute top-9 z-10">
         <img src={logo} alt="logo" className="w-12" />
       </a>
       <div
-        className={`transition-all w-full h-full ${
-          transitioning
-            ? "transform -translate-x-[150%] duration-1000 ease"
-            : ""
+        className={`transition-transform ease-in-out duration-500 w-full h-full ${
+          transitioning ? "slide-in" : "slide-out"
         }`}
       >
         {
           {
             1: (
               <div
+                id="one"
                 onClick={() => handleStepChange(2)}
                 className="w-full h-full flex flex-col items-center justify-center"
               >
@@ -40,9 +39,19 @@ function App() {
                 </p>
               </div>
             ),
-            2: <Waitlist handleSetStep={handleStepChange} />,
+            2: (
+              <div
+                id="two"
+                className="w-full h-full flex flex-col items-center justify-center"
+              >
+                <Waitlist handleSetStep={handleStepChange} />
+              </div>
+            ),
             3: (
-              <div className="w-full h-full flex flex-col items-center justify-center">
+              <div
+                id="three"
+                className="w-full h-full flex flex-col items-center justify-center"
+              >
                 <img src={admit} alt="admit" />
                 <p className="text-[#7E7E7E] text-center mt-4 text-base">
                   See you at the onboarding
